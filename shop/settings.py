64813a6c70c08df039596431676411cd4be69107
Nikita Averkin <awerk1n2000@mail.ru>
2026-01-04 15:46:49 +0800

    Сделал Docker контейнер

--- shop/settings.py
+++ shop/settings.py
@@ -30,8 +30,20 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'driftwoodbrand.ru', 'www.driftwoodbrand.ru']
 
+CSRF_TRUSTED_ORIGINS = [
+    'http://127.0.0.1:8000'
+    'http://localhost:8000'
+    'http://driftwoodbrand.ru'
+    'http://www.driftwoodbrand.ru'
+]
+
+CSRF_COOKIE_SECURE = True 
+SESSION_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'
 
 # Application definition
 
